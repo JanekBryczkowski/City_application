5fb2e9bb0fd0536c1a478b5563546bd356f77f37
zuzajer <zuzajer@gmail.com>
2020-02-25 15:17:29 +0100

    miasto class

---
+++ src/city/Town.java
@@ -0,0 +1,50 @@
+package city;
+
+import java.util.List;
+
+public class Town {
+
+    private String name;
+    private Integer x;
+    private Integer y;
+    private List connections;
+
+    public Town(String name, Integer x, Integer y, List connections) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.connections = connections;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public List getConnections() {
+        return connections;
+    }
+
+    public void setConnections(List connections) {
+        this.connections = connections;
+    }
+}
